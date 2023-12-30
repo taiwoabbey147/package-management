102944dd84350ddde4ac67c02d1097d83f3c3d38
taiwoabbey147 <142275470+taiwoabbey147@users.noreply.github.com>
2023-12-30 14:39:23 -0600

    Update Jenkins-redhat8-install28.sh

--- JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
@@ -3,7 +3,7 @@
 # date: sep/12/2022
 # Installing Jenkins on RHEL 7/8, CentOS 7/8 or Amazon Linux OS
 # You can execute this script as user-data when launching your EC2 VM.
-sudo timedatectl set-timezone America/New_York
+sudo timedatectl set-timezone America/Chicago
 sudo hostnamectl set-hostname jenkins
 sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
